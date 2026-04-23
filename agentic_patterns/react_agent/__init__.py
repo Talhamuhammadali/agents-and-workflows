27b09baa81c93d24c3014122eb45a8333f20470e
Talhamuhammadali <talha_muhammadali@hotmail.com>
2026-04-23 16:55:56 +0500

    [refactor]

--- agentic_patterns/react_agent/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Basic implementation of the React Agent pattern will be reused for other agent patterns."""
--- agentic_patterns/react_agent/tools.py
+++
@@ -1,5 +0,0 @@
-"""Tools available to the React agent."""
-
-from agentic_patterns.shared import BASH_TOOLS, FILE_TOOLS, ask_question, update_todos
-
-TOOLS: list = [update_todos, ask_question, *BASH_TOOLS, *FILE_TOOLS]
